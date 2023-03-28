fb17a63c466742d82d93b502d1b7781a94074107
Adam Szmagliński <aszmag@hotmail.com>
2023-03-28 11:16:24 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Wykres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.labelY = new System.Windows.Forms.Label();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(638, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rysuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelX
+            // 
+            this.labelX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(729, 9);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(30, 13);
+            this.labelX.TabIndex = 1;
+            this.labelX.Text = "Oś X";
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownX.Location = new System.Drawing.Point(732, 25);
+            this.numericUpDownX.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDownX.TabIndex = 2;
+            this.numericUpDownX.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownX.ValueChanged += new System.EventHandler(this.numericUpDownX_ValueChanged);
+            // 
+            // labelY
+            // 
+            this.labelY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(729, 63);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(30, 13);
+            this.labelY.TabIndex = 3;
+            this.labelY.Text = "Oś Y";
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownY.Location = new System.Drawing.Point(732, 79);
+            this.numericUpDownY.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDownY.TabIndex = 4;
+            this.numericUpDownY.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownY.ValueChanged += new System.EventHandler(this.numericUpDownY_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.numericUpDownX);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Wykres";
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wykres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rysuj();
+        }
+
+        private void Rysuj()
+        {
+            this.Invalidate();
+            Application.DoEvents();
+            Graphics g = this.CreateGraphics();
+
+            int szerokosc = (int)g.VisibleClipBounds.Width;
+            int wysokosc = (int)g.VisibleClipBounds.Height;
+            if (wysokosc < 47) return;
+            Bitmap b = new Bitmap(szerokosc, wysokosc);
+
+            int d = 40; // odsunięcie osi od krawędzi
+            int m = 10; // margines
+            int zeroX = d; // punkt 0 na X (położenie osi Y) w pikselach
+            int zeroY = wysokosc - d; // punkt 0 na Y (położenie osi X) w pikselach
+            int xMax = (int)numericUpDownX.Value; // maskymalna wartość argumentu
+            int yMax = (int)numericUpDownY.Value;
+            int jednostkaX = (szerokosc - d - zeroX) / xMax; // jednostka osi X w pikselach
+            int jednostkaY = (zeroY - d) / yMax;
+
+            for (int i = m; i <= szerokosc - m; i++) b.SetPixel(i, zeroY, Color.Black); // rysuje oś X
+            for (int i = m; i <= wysokosc - m; i++) b.SetPixel(zeroX, i, Color.Black); // rysuje oś Y
+            for (int i = 3; i < 20; i++)
+            {
+                b.SetPixel(szerokosc - m - i, zeroY + i * 2 / 5, Color.Black); // grot dolny osi X
+                b.SetPixel(szerokosc - m - i, zeroY - i * 2 / 5, Color.Black); // grot górny osi X
+                b.SetPixel(zeroX + i * 2 / 5, m + i, Color.Black); // grot prawy osi Y
+                b.SetPixel(zeroX - i * 2 / 5, m + i, Color.Black); // grot lewy osi Y
+            }
+            for (int i = 1; i <= xMax; i++) // skala osi X
+                for (int j = -5; j <= 5; j++)
+                    b.SetPixel(zeroX + i * jednostkaX, zeroY + j, Color.Black);
+            for (int i = 1; i <= yMax; i++) // skala osi Y
+                for (int j = -5; j <= 5; j++)
+                    b.SetPixel(zeroX + j, zeroY - i * jednostkaY, Color.Black);
+
+            float dx = 1 / (jednostkaX * 10.0f);
+            for (float x = 0; x <= xMax; x += dx)
+            {
+                int xx = zeroX + (int)(x * jednostkaX);
+                int yy = zeroY - (int)(f(x) * jednostkaY);
+                if (0 <= yy && yy <= wysokosc) b.SetPixel(xx, yy, Color.Blue);
+            }
+
+            g.DrawImage(b, 0, 0);
+
+            for (int i = 1; i <= xMax; i++) g.DrawString(i.ToString(), Font, Brushes.Black, zeroX - 5 + i * jednostkaX, zeroY + 8);
+            for (int i = 1; i <= yMax; i++) g.DrawString(i.ToString(), Font, Brushes.Black, zeroX - 20, zeroY - 6 - i * jednostkaY);
+        }
+
+        private float f(float x)
+        {
+            return (float)(x * Math.Sin(x) * Math.Sin(x));
+        }
+
+        private void numericUpDownX_ValueChanged(object sender, EventArgs e)
+        {
+            Rysuj();
+        }
+
+        private void numericUpDownY_ValueChanged(object sender, EventArgs e)
+        {
+            Rysuj();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Rysuj();
+        }
+
+        private int x1 = 0, y1 = 0;
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            int x = this.Location.X, y = this.Location.Y;
+            if (x1 < 0 && x1 < x || Screen.PrimaryScreen.WorkingArea.Width < x1 + this.Size.Width && x < x1 || Screen.PrimaryScreen.WorkingArea.Height < y1 + this.Size.Height && y < y1) Rysuj();
+            x1 = x; y1 = y;
+        }
+    }
+}
